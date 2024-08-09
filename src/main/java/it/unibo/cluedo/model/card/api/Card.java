d1d51de2e8898331fb672472f84e812f9870b166
Marco Paggetti <marco.paggetti@studio.unibo.it>
2024-08-09 10:31:18 +0200

    Added Card interface

---
+++ src/main/java/it/unibo/cluedo/model/card/api/Card.java
@@ -0,0 +1,28 @@
+package it.unibo.cluedo.model.card.api;
+
+public interface Card {
+    
+    /**
+     * Enum representing the different types of cards in the game Cluedo.
+     */
+    enum Type {
+        /**
+         * Represents a character card.
+         */
+        CHARACTER,
+        /**
+         * Represents a weapon card.
+         */
+        WEAPON,
+        /**
+         * Represents a room card.
+         */
+        ROOM
+    }
+
+    Type getType();
+
+    String getName();
+
+    Object getImage();
+}
