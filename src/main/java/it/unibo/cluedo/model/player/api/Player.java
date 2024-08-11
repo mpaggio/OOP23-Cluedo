63b0ed795e3c3c5b3930a9872253a102d0e5ad8b
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-08-12 00:19:43 +0200

    Added some methods in player interface

--- src/main/java/it/unibo/cluedo/model/player/api/Player.java
+++ src/main/java/it/unibo/cluedo/model/player/api/Player.java
@@ -1,6 +1,9 @@
 package it.unibo.cluedo.model.player.api;
 
+import it.unibo.cluedo.model.card.api.Card;
 import it.unibo.cluedo.utilities.Position;
+import java.util.List;
+
 
 /**
  * Interface wich models a player. Each player has a username, position
@@ -12,40 +15,51 @@ public interface Player {
 
     /**
      * Get the username of a certain player.
-     * 
      * @return a string which is the username of the player
      */
     String getUsername();
 
     /**
      * Get the current position of the player.
-     * 
      * @return the position.
      */
     Position getCurrentPosition();
 
     /**
+     * Check if it is current's player turn.
+     * @return
+     */
+    boolean isPlayerTurn();
+
+    /**
      * Move the player to a certain position.
-     * 
      * @param coords - position where to move the player
      */
     void setPosition(Position coords);
 
     /**
      * Get the color of the player.
-     * 
      * @return a string which is the color of the player
      */
     String getColor();
 
     /**
+     * Checks if the player is in a room
      * @return a boolean which indicates if a player is in a room.
      */
     boolean isInRoom();
 
     /**
+     * Checks if the player has won.
      * @return a boolean which indicates if a player has won or not.
      */
     boolean hasWon();
+
+    /**
+     * Return the list of cards owned by the player.
+     * @return
+     */
+    List<Card> getPlayerCards();
+
 
 }
