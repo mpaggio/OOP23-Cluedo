b13376e276a9d69422b3d40d1b2363345ffad582
Marco Paggetti <marco.paggetti@studio.unibo.it>
2024-08-15 13:20:38 +0200

    Fixed some checkstyle and pmd errors

--- src/main/java/it/unibo/cluedo/model/map/api/Map.java
+++ src/main/java/it/unibo/cluedo/model/map/api/Map.java
@@ -1,5 +1,7 @@
 package it.unibo.cluedo.model.map.api;
 
+/**
+ * Represents the map of the game of Cluedo.
+ */
 public interface Map {
-    
 }
--- src/main/java/it/unibo/cluedo/model/map/impl/MapImpl.java
+++ src/main/java/it/unibo/cluedo/model/map/impl/MapImpl.java
@@ -9,39 +9,46 @@ import it.unibo.cluedo.model.square.impl.NoEffectImpl;
 import it.unibo.cluedo.model.square.impl.SquareImpl;
 import it.unibo.cluedo.utilities.Position;
 
+/**
+ * Implementation of the Map interface for the Cluedo game.
+ * This class represents the game map and initializes the rooms and squares based
+ * on a predefined layout.
+ */
 public class MapImpl implements Map {
     private static final int MAP_HEIGHT = 25;
     private static final int MAP_WIDTH = 24;
-    private static final int[][] MAP_TILES_DISPOSITION = new int[][]
-        {
-            {0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,0,0,0,0,0},
-            {2,2,2,2,2,4,0,1,1,1,5,5,5,5,1,1,1,0,6,6,6,6,6,6},
-            {2,2,2,2,2,2,1,1,5,5,5,5,5,5,5,5,1,1,6,6,6,6,6,6},
-            {2,2,2,2,2,2,1,1,5,5,5,5,5,5,5,5,1,1,6,6,6,6,6,6},
-            {2,2,2,2,2,2,1,1,5,5,5,5,5,5,5,5,1,1,3,6,6,6,6,6},
-            {2,2,2,2,2,2,1,1,3,5,5,5,5,5,5,3,1,1,1,6,6,6,4,0},
-            {0,2,2,2,3,2,1,1,5,5,5,5,5,5,5,5,1,1,1,1,1,1,1,1},
-            {1,1,1,1,1,1,1,1,5,3,5,5,5,5,3,5,1,1,1,1,1,1,1,0},
-            {0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,7,7,7,7,7,7},
-            {12,12,12,12,12,1,1,1,1,1,1,1,1,1,1,1,1,1,3,7,7,7,7,7},
-            {12,12,12,12,12,12,12,12,1,1,2,2,2,2,2,1,1,1,7,7,7,7,7,7},
-            {12,12,12,12,12,12,12,12,1,1,2,2,2,2,2,1,1,1,7,7,7,7,7,7},
-            {12,12,12,12,12,12,12,12,1,1,2,2,2,2,2,1,1,1,7,7,7,7,3,7},
-            {12,12,12,12,12,12,12,3,1,1,2,2,2,2,2,1,1,1,1,1,1,1,1,0},
-            {12,12,12,12,12,12,12,12,1,1,2,2,2,2,2,1,1,1,8,8,3,8,8,0},
-            {12,12,12,12,12,12,12,12,1,1,2,2,2,2,2,1,1,8,8,8,8,8,8,8},
-            {12,12,12,12,12,12,3,12,1,1,2,2,2,2,2,1,1,3,8,8,8,8,8,8},
-            {0,1,1,1,1,1,1,1,1,1,2,2,3,2,2,1,1,8,8,8,8,8,8,8},
-            {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,8,8,8,8,8,0},
-            {0,1,1,1,1,1,1,1,1,10,10,3,3,10,10,1,1,1,1,1,1,1,1,1},
-            {4,11,11,11,11,11,3,1,1,10,10,10,10,10,10,1,1,1,1,1,1,1,1,0},
-            {11,11,11,11,11,11,11,1,1,10,10,10,10,10,10,1,1,3,9,9,9,9,9,4},
-            {11,11,11,11,11,11,11,1,1,10,10,10,10,10,10,1,1,9,9,9,9,9,9,9},
-            {11,11,11,11,11,11,11,1,1,10,10,10,10,10,10,1,1,9,9,9,9,9,9,9},
-            {11,11,11,11,11,11,0,1,0,10,10,10,10,10,10,0,1,0,9,9,9,9,9,9}
-        };  
+    private static final int[][] MAP_TILES_DISPOSITION = {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {2, 2, 2, 2, 2, 4, 0, 1, 1, 1, 5, 5, 5, 5, 1, 1, 1, 0, 6, 6, 6, 6, 6, 6},
+            {2, 2, 2, 2, 2, 2, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 6, 6, 6, 6, 6, 6},
+            {2, 2, 2, 2, 2, 2, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 6, 6, 6, 6, 6, 6},
+            {2, 2, 2, 2, 2, 2, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 3, 6, 6, 6, 6, 6},
+            {2, 2, 2, 2, 2, 2, 1, 1, 3, 5, 5, 5, 5, 5, 5, 3, 1, 1, 1, 6, 6, 6, 4, 0},
+            {0, 2, 2, 2, 3, 2, 1, 1, 5, 5, 5, 5, 5, 5, 5, 5, 1, 1, 1, 1, 1, 1, 1, 1},
+            {1, 1, 1, 1, 1, 1, 1, 1, 5, 3, 5, 5, 5, 5, 3, 5, 1, 1, 1, 1, 1, 1, 1, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 7, 7, 7, 7, 7, 7},
+            {12, 12, 12, 12, 12, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 7, 7, 7, 7, 7},
+            {12, 12, 12, 12, 12, 12, 12, 12, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 7, 7, 7, 7, 7, 7},
+            {12, 12, 12, 12, 12, 12, 12, 12, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 7, 7, 7, 7, 7, 7},
+            {12, 12, 12, 12, 12, 12, 12, 12, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 7, 7, 7, 7, 3, 7},
+            {12, 12, 12, 12, 12, 12, 12, 3, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {12, 12, 12, 12, 12, 12, 12, 12, 1, 1, 2, 2, 2, 2, 2, 1, 1, 1, 8, 8, 3, 8, 8, 0},
+            {12, 12, 12, 12, 12, 12, 12, 12, 1, 1, 2, 2, 2, 2, 2, 1, 1, 8, 8, 8, 8, 8, 8, 8},
+            {12, 12, 12, 12, 12, 12, 3, 12, 1, 1, 2, 2, 2, 2, 2, 1, 1, 3, 8, 8, 8, 8, 8, 8},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 3, 2, 2, 1, 1, 8, 8, 8, 8, 8, 8, 8},
+            {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 8, 8, 8, 8, 8, 0},
+            {0, 1, 1, 1, 1, 1, 1, 1, 1, 10, 10, 3, 3, 10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 1},
+            {4, 11, 11, 11, 11, 11, 3, 1, 1, 10, 10, 10, 10, 10, 10, 1, 1, 1, 1, 1, 1, 1, 1, 0},
+            {11, 11, 11, 11, 11, 11, 11, 1, 1, 10, 10, 10, 10, 10, 10, 1, 1, 3, 9, 9, 9, 9, 9, 4},
+            {11, 11, 11, 11, 11, 11, 11, 1, 1, 10, 10, 10, 10, 10, 10, 1, 1, 9, 9, 9, 9, 9, 9, 9},
+            {11, 11, 11, 11, 11, 11, 11, 1, 1, 10, 10, 10, 10, 10, 10, 1, 1, 9, 9, 9, 9, 9, 9, 9},
+            {11, 11, 11, 11, 11, 11, 0, 1, 0, 10, 10, 10, 10, 10, 10, 0, 1, 0, 9, 9, 9, 9, 9, 9}
+        };
     private List<MapComponent> tiles;
 
+    /**
+     * Enum representing the different types of room in the Cluedo game.
+     */
     private enum RoomType {
         KITCHEN(2, "Kitchen"),
         BALLROOM(5, "Ballroom"),
@@ -56,22 +63,45 @@ public class MapImpl implements Map {
         private final int code;
         private final String name;
 
+        /**
+         * Constructor for RoomType.
+         * 
+         * @param code the code of the room type
+         * @param name the name of the room type
+         */
         RoomType(final int code, final String name) {
             this.code = code;
             this.name = name;
         }
 
+        /**
+         * Gets the code of the room type.
+         * 
+         * @return the code of the room type 
+         */
         public int getCode() {
             return this.code;
         }
 
+        /**
+         * Gets the name of the room type.
+         * 
+         * @return the name of the room type
+         */
         public String getName() {
             return this.name;
         }
 
+        /**
+         * Returns the room type corresponding to the given code.
+         * 
+         * @param code the code of the room type
+         * @return the RoomType corresponding to the given code
+         * @throws IllegalArgumentException if the code does not correspond to any RoomType
+         */
         public static RoomType fromCode(final int code) {
-            for(RoomType type : values()){
-                if(type.getCode() == code) {
+            for (final RoomType type : values()) {
+                if (type.getCode() == code) {
                     return type;
                 }
             }
@@ -79,20 +109,24 @@ public class MapImpl implements Map {
         }
     }
 
+    /**
+     * Constructor for MapImpl.
+     * Initializes the rooms and adds the square to the map based on the predefined layout.
+     */
     public MapImpl() {
         final RoomImpl[] rooms = new RoomImpl[RoomType.values().length];
         // Initialising rooms
-        for(RoomType type : RoomType.values()) {
+        for (final RoomType type : RoomType.values()) {
             rooms[type.ordinal()] =  new RoomImpl(type.getName());
             this.tiles.add(rooms[type.ordinal()]);
         }
         // Adding squares and assigning to rooms if necessary
-        for(int i = 0; i < MAP_HEIGHT; i ++) {
-            for(int j = 0; j < MAP_WIDTH; j++) {
+        for (int i = 0; i < MAP_HEIGHT; i++) {
+            for (int j = 0; j < MAP_WIDTH; j++) {
                 final int tileType =  MAP_TILES_DISPOSITION[i][j];
-                if(tileType == 1) {
+                if (tileType == 1) {
                     tiles.add(new SquareImpl(new Position(i, j), new NoEffectImpl()));
-                } else if(tileType == 3) {
+                } else if (tileType == 3) {
                     rooms[findRoomForEntrance(i, j).ordinal()].addEntrance(
                         new SquareImpl(new Position(i, j), new NoEffectImpl())
                     );
@@ -105,14 +139,21 @@ public class MapImpl implements Map {
         } 
     }
 
-    private RoomType findRoomForEntrance(final int i, final int j){
-        if(i > 0 && MAP_TILES_DISPOSITION[i - 1][j] != 1 &&  MAP_TILES_DISPOSITION[i - 1][j] != 3) {
+    /**
+     * Finds the room type for the entrance at the given position.
+     * 
+     * @param i the row index of the entrance
+     * @param j the column index of the entrance
+     * @return the room type corresponding to the entrance position
+     */
+    private RoomType findRoomForEntrance(final int i, final int j) {
+        if (i > 0 && MAP_TILES_DISPOSITION[i - 1][j] != 1 &&  MAP_TILES_DISPOSITION[i - 1][j] != 3) {
             return RoomType.fromCode(MAP_TILES_DISPOSITION[i - 1][j]);
-        } else if(i < MAP_HEIGHT - 1 && MAP_TILES_DISPOSITION[i + 1][j] != 1 &&  MAP_TILES_DISPOSITION[i + 1][j] != 3) {
+        } else if (i < MAP_HEIGHT - 1 && MAP_TILES_DISPOSITION[i + 1][j] != 1 &&  MAP_TILES_DISPOSITION[i + 1][j] != 3) {
             return RoomType.fromCode(MAP_TILES_DISPOSITION[i + 1][j]);
-        } else if(j > 0 && MAP_TILES_DISPOSITION[i][j - 1] != 1 &&  MAP_TILES_DISPOSITION[i][j - 1] != 3) {
+        } else if (j > 0 && MAP_TILES_DISPOSITION[i][j - 1] != 1 &&  MAP_TILES_DISPOSITION[i][j - 1] != 3) {
             return RoomType.fromCode(MAP_TILES_DISPOSITION[i][j - 1]);
-        } else if(j < MAP_WIDTH - 1 && MAP_TILES_DISPOSITION[i][j + 1] != 1 &&  MAP_TILES_DISPOSITION[i][j + 1] != 3) {
+        } else if (j < MAP_WIDTH - 1 && MAP_TILES_DISPOSITION[i][j + 1] != 1 &&  MAP_TILES_DISPOSITION[i][j + 1] != 3) {
             return RoomType.fromCode(MAP_TILES_DISPOSITION[i][j + 1]);
         } else {
             return null;
