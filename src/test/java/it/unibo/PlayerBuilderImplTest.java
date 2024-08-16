64d95dbfa3f2e328754d81d5d38f96d96eba8145
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-08-16 13:55:40 +0200

    Added test classe for player builder implementation

---
+++ src/test/java/it/unibo/PlayerBuilderImplTest.java
@@ -0,0 +1,40 @@
+package it.unibo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+import it.unibo.cluedo.model.player.api.Player;
+import it.unibo.cluedo.model.player.impl.PlayerBuilderImpl;
+
+/**
+ * Test class for a {@link PlayerBuilderImpl} class
+ */
+public class PlayerBuilderImplTest {
+
+    /**
+     * Check if the player is created correctly
+     */
+    @Test
+    void testBuildPlayer() {
+        final Player player = new PlayerBuilderImpl().username("username").color("color").buildPlayer();
+        assertNotNull(player);
+        assertEquals("username", player.getUsername());
+        assertEquals("color", player.getColor());
+    }
+
+    /**
+     * Check that building a player without not enough information throws an exception or fails
+     */
+    @Test
+    void testBuildPlayerWithoutEnoughInformation() {
+        final String username = "username";
+        final PlayerBuilderImpl playerBuilder = new PlayerBuilderImpl();
+        assertThrows(IllegalArgumentException.class,
+                () -> {
+                    playerBuilder.username(username)
+                            .buildPlayer();
+                });
+    }
+
+}
