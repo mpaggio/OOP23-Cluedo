ad130dc46f52269170e82ad756978f90f162741e
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2024-08-16 11:22:58 +0200

    In the test class for the statistics (StatisticsTest.Java) added the test for the number of romms visited by each player. This test verify the correct behavour of the incrementRoomsVisited method and its leaderboard.

--- src/test/java/it/unibo/StatisticsTest.java
+++ src/test/java/it/unibo/StatisticsTest.java
@@ -18,14 +18,21 @@ import java.util.LinkedHashMap;
 /**
  * This class contains the tests for the Statistics class.
  */
-final class StatisticsTest { 
+final class StatisticsTest {
+
     private static final int STEPS_FOR_PLAYER1_1 = 5;
     private static final int STEPS_FOR_PLAYER1_2 = 2;
     private static final int STEPS_FOR_PLAYER1 = 7;
     private static final int STEPS_FOR_PLAYER2 = 2;
     private static final int STEPS_FOR_PLAYER3 = 3;
+
     private List<Player> players;
     private Statistics stats;
+    private Map<Player, Integer> leaderboard;
+
+    /**
+     * Set up the players and the statistics before each test.
+     */
     @BeforeEach
     void setUp() {
         players = new ArrayList<>();
@@ -33,8 +40,13 @@ final class StatisticsTest {
         players.add(new PlayerImpl("Player2", "Red"));
         players.add(new PlayerImpl("Player3", "Green"));
         stats = new StatisticsImpl(players);
+        leaderboard = new LinkedHashMap<>();
     }
 
+    /**
+     * Test that the steps are correctly incremented by the method incrementSteps and
+     * weather the leaderboard is correct.
+     */
     @Test
     void testSteps() {
         stats.incrementSteps(players.get(0), STEPS_FOR_PLAYER1_1);
@@ -45,10 +57,28 @@ final class StatisticsTest {
         assertEquals(stats.getStepsMade().get(players.get(1)), STEPS_FOR_PLAYER2);
         assertEquals(stats.getStepsMade().get(players.get(2)), STEPS_FOR_PLAYER3);
         //Expected leaderboard: Player1, Player3, Player2
-        final Map<Player, Integer> leaderboard = new LinkedHashMap<>();
         leaderboard.put(players.get(0), STEPS_FOR_PLAYER1);
         leaderboard.put(players.get(2), STEPS_FOR_PLAYER3);
         leaderboard.put(players.get(1), STEPS_FOR_PLAYER2);
         assertEquals(leaderboard, stats.getStepsMade());
+    }
+
+    /**
+     * Test that the steps are correctly incremented by the method incrementRoomsVisited and
+     * weather the leaderboard is correct.
+     */
+    @Test
+    void testRoomVisited() {
+        stats.incrementRoomsVisited(players.get(0));
+        stats.incrementRoomsVisited(players.get(0));
+        stats.incrementRoomsVisited(players.get(1));
+        assertEquals(stats.getRoomsVisited().get(players.get(0)), 2);
+        assertEquals(stats.getRoomsVisited().get(players.get(1)), 1);
+        assertEquals(stats.getRoomsVisited().get(players.get(2)), 0);
+        //Expected leaderboard: Player1, Player2, Player3
+        leaderboard.put(players.get(0), 2);
+        leaderboard.put(players.get(1), 1);
+        leaderboard.put(players.get(2), 0);
+        assertEquals(stats.getRoomsVisited(), leaderboard);
     }
 }
