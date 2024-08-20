db6e8d5ef5a9a80a6b89270bb02c7cee2a17bc1b
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-08-20 14:53:25 +0200

    Modified class MoveInCombinedDirectionsCommand, now the player can move in multiple directions

--- src/main/java/it/unibo/cluedo/model/movement/impl/MoveInCombinedDirectionsCommand.java
+++ src/main/java/it/unibo/cluedo/model/movement/impl/MoveInCombinedDirectionsCommand.java
@@ -4,68 +4,74 @@ package it.unibo.cluedo.model.movement.impl;
 import it.unibo.cluedo.model.movement.api.MovementCommand;
 import it.unibo.cluedo.model.movement.api.MovementStrategy;
 import it.unibo.cluedo.model.player.api.MutablePlayer;
-import it.unibo.cluedo.utilities.Position;
-
+import it.unibo.cluedo.model.player.api.Player;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 /**
  * Command for moving a player in combined directions.
  */
 public final class MoveInCombinedDirectionsCommand implements MovementCommand {
 
-    private final MutablePlayer player;
-    private final int verticalSteps;
-    private final int horizontalSteps;
-    private final MovementStrategy.Direction verticalDirection;
-    private final MovementStrategy.Direction horizontalDirection;
+    private final Player player;
+    private final List<Movement> movements;
     private final MovementStrategy movementStrategy;
 
     /**
-     * Constructs a new command for moving the player in combined directions.
-     *
-     * @param player the player to be moved
-     * @param verticalSteps the number of steps to move vertically
-     * @param horizontalSteps the number of steps to move horizontally
-     * @param verticalDirection the vertical direction (UP or DOWN)
-     * @param horizontalDirection the horizontal direction (LEFT or RIGHT)
+     * Represents a single movement step.
+     */
+    public static class Movement {
+        private final int steps;
+        private final MovementStrategy.Direction direction;
+
+        /**
+         * Construct a new movement.
+         * @param steps the number of steps to move on
+         * @param direction the direction where to move (UP, DOWN, LEFT, RIGHT)
+         */
+        public Movement(final int steps, final MovementStrategy.Direction direction) {
+            this.steps = steps;
+            this.direction = direction;
+        }
+
+        /**
+         * {@inheritDoc}
+         * @return the number of steps to move on
+         */
+        public int getSteps() {
+            return steps;
+        }
+
+        /**
+         * {@inheritDoc}
+         * @return the direction where to move
+         */
+        public MovementStrategy.Direction getDirection() {
+            return direction;
+        }
+
+    }
+
+    /**
+     * Constructor a new command for moving the player in multiple directions.
+     * @param player player to move
+     * @param movements the list of movements to perform
      * @param movementStrategy the strategy used to calculate and validate the movement
      */
-    public MoveInCombinedDirectionsCommand(final MutablePlayer player, final int verticalSteps, final int horizontalSteps,
-                                           final MovementStrategy.Direction verticalDirection,
-                                           final MovementStrategy.Direction horizontalDirection,
-                                           final MovementStrategy movementStrategy) {
+    public MoveInCombinedDirectionsCommand(final MutablePlayer player, final List<Movement> movements,
+                                        final MovementStrategy movementStrategy) {
         this.player = player.clone();
-        this.verticalSteps = verticalSteps;
-        this.horizontalSteps = horizontalSteps;
-        this.verticalDirection = verticalDirection;
-        this.horizontalDirection = horizontalDirection;
+        this.movements = Collections.unmodifiableList(new ArrayList<>(movements));
         this.movementStrategy = movementStrategy;
     }
 
     @Override
     public void execute() {
-        // Movimento verticale
-        Position newPosition = movementStrategy.calculatePosition(
-            player.getCurrentPosition(), verticalSteps, verticalDirection
-        );
-
-        if (movementStrategy.isValidMove(player, newPosition, 10)) {  // Supponendo una scacchiera 10x10
-            player.setPosition(newPosition);
-        } else {
-            throw new IllegalArgumentException(
-                "Invalid vertical move: The player cannot move outside the board or into an invalid area."
-            );
-        }
-
-        // Movimento orizzontale
-        newPosition = movementStrategy.calculatePosition(
-            player.getCurrentPosition(), horizontalSteps, horizontalDirection
-        );
-
-        if (movementStrategy.isValidMove(player, newPosition, 10)) {
-            player.setPosition(newPosition);
-        } else {
-            throw new IllegalArgumentException(
-                "Invalid horizontal move: The player cannot move outside the board or into an invalid area."
-            );
+        for (final var movement : movements) {
+            final MovementCommand singleMovementCommand = new MoveInSingleDirection(
+                (MutablePlayer) player, movement.getSteps(), movement.getDirection(), movementStrategy);
+            singleMovementCommand.execute();
         }
     }
+
 }
