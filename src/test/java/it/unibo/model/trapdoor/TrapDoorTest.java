bc63d91f8d838db8f344061d4a9e5190adbdc783
Marco Paggetti <marco.paggetti@studio.unibo.it>
2024-08-20 10:51:13 +0200

    Added base for trapdoor test

---
+++ src/test/java/it/unibo/model/trapdoor/TrapDoorTest.java
@@ -0,0 +1,39 @@
+package it.unibo.model.trapdoor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.cluedo.model.room.api.Room;
+import it.unibo.cluedo.model.room.impl.RoomImpl;
+import it.unibo.cluedo.model.trapdoor.api.TrapDoor;
+import it.unibo.cluedo.model.trapdoor.impl.TrapDoorImpl;
+import it.unibo.cluedo.utilities.Position;
+
+/**
+ * Test class for SquareImpl, SquareFactory and Effect implementations.
+ */
+final class TrapDoorTest {
+    Room room;
+    Position position;
+    TrapDoor trapDoor;
+
+    /**
+     * Initialization before every test.
+     */
+    @BeforeEach
+    void setUp() {
+        this.room = new RoomImpl("Kitchen");
+        this.position = new Position(0, 0);
+        this.trapDoor = new TrapDoorImpl(this.room, this.position);  
+    }
+
+    @Test
+    void testTrapDoor() {
+        assertNotEquals(this.room, this.trapDoor.getConnectedRoom());
+        assertEquals(this.room.getName(),this.trapDoor.getConnectedRoom().getName());
+        assertEquals(this.position, this.trapDoor.getPosition());
+    }
+}
