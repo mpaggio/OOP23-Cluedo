e8a44107451b6f537a57cbea1b69424e95958028
Federico Brighi <federico.brighi2@studio.unibo.it>
2024-08-23 13:41:41 +0200

    modified some methods name in order of new implementation

--- src/main/java/it/unibo/cluedo/model/unforseen/api/Unforseen.java
+++ src/main/java/it/unibo/cluedo/model/unforseen/api/Unforseen.java
@@ -2,19 +2,19 @@ package it.unibo.cluedo.model.unforseen.api;
 import it.unibo.cluedo.model.player.api.Player;
 
 /**
- * Interface that represents an event card in the Cluedo game.
+ * Interface that represents an unforseen card in the Cluedo game.
  */
 public interface Unforseen {
     /**
-     * Executes the action of the event card.
+     * Apply the unforseen card's effect to the given player.
      *
-     * @param player the player who triggered the event card
+     * @param player the player who triggered the unforseen card
      */
-    void applyEffetct(Player player);
+    void apply(Player player);
 
     /**
-     * Returns the name of the event card.
-     * @return the name of the event card
+     * Returns the description of the event card.
+     * @return the description of the event card
      */
-    String getName();
+    String getDescription();
 }
