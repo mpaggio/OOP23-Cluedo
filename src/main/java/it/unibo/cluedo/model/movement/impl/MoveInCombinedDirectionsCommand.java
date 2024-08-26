a39488664ea35b90adedeac0e7efdc4e904c3cdb
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-08-26 14:31:16 +0200

    Modified class MoveInCombinedDirectionsCommand to move the player one step at a time in multiple directions

--- src/main/java/it/unibo/cluedo/model/movement/impl/MoveInCombinedDirectionsCommand.java
+++ src/main/java/it/unibo/cluedo/model/movement/impl/MoveInCombinedDirectionsCommand.java
@@ -6,7 +6,6 @@ import it.unibo.cluedo.model.movement.api.MovementStrategy;
 import it.unibo.cluedo.model.player.api.MutablePlayer;
 import it.unibo.cluedo.model.player.api.Player;
 import java.util.List;
-import java.util.ArrayList;
 import java.util.Collections;
 /**
  * Command for moving a player in combined directions.
@@ -14,62 +13,28 @@ import java.util.Collections;
 public final class MoveInCombinedDirectionsCommand implements MovementCommand {
 
     private final Player player;
-    private final List<Movement> movements;
+    private final List<MovementStrategy.Direction> directions;
     private final MovementStrategy movementStrategy;
-
-    /**
-     * Represents a single movement step.
-     */
-    public static class Movement {
-        private final int steps;
-        private final MovementStrategy.Direction direction;
-
-        /**
-         * Construct a new movement.
-         * @param steps the number of steps to move on
-         * @param direction the direction where to move (UP, DOWN, LEFT, RIGHT)
-         */
-        public Movement(final int steps, final MovementStrategy.Direction direction) {
-            this.steps = steps;
-            this.direction = direction;
-        }
-
-        /**
-         * {@inheritDoc}
-         * @return the number of steps to move on
-         */
-        public int getSteps() {
-            return steps;
-        }
-
-        /**
-         * {@inheritDoc}
-         * @return the direction where to move
-         */
-        public MovementStrategy.Direction getDirection() {
-            return direction;
-        }
-
-    }
+    private static final int STEPS = 1;
 
     /**
      * Constructor a new command for moving the player in multiple directions.
      * @param player player to move
-     * @param movements the list of movements to perform
+     * @param directions list of directions where to move
      * @param movementStrategy the strategy used to calculate and validate the movement
      */
-    public MoveInCombinedDirectionsCommand(final MutablePlayer player, final List<Movement> movements,
+    public MoveInCombinedDirectionsCommand(final Player player, final List<MovementStrategy.Direction> directions,
                                         final MovementStrategy movementStrategy) {
-        this.player = player.clone();
-        this.movements = Collections.unmodifiableList(new ArrayList<>(movements));
+        this.player = player;
+        this.directions = Collections.unmodifiableList(directions);
         this.movementStrategy = movementStrategy;
     }
 
     @Override
     public void execute() {
-        for (final var movement : movements) {
+        for (final var direction : directions) {
             final MovementCommand singleMovementCommand = new MoveInSingleDirection(
-                (MutablePlayer) player, movement.getSteps(), movement.getDirection(), movementStrategy);
+                (MutablePlayer) this.player, STEPS, direction, movementStrategy);
             singleMovementCommand.execute();
         }
     }
