4f33ef85d3545d14f9de2586f76325681e25e3ce
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-08-26 16:18:05 +0200

    Modified class MoveInCombinedDirectionsCommand

--- src/main/java/it/unibo/cluedo/model/movement/impl/MoveInCombinedDirectionsCommand.java
+++ src/main/java/it/unibo/cluedo/model/movement/impl/MoveInCombinedDirectionsCommand.java
@@ -5,38 +5,34 @@ import it.unibo.cluedo.model.movement.api.MovementCommand;
 import it.unibo.cluedo.model.movement.api.MovementStrategy;
 import it.unibo.cluedo.model.player.api.MutablePlayer;
 import it.unibo.cluedo.model.player.api.Player;
-import java.util.List;
-import java.util.Collections;
 /**
  * Command for moving a player in combined directions.
  */
 public final class MoveInCombinedDirectionsCommand implements MovementCommand {
 
     private final Player player;
-    private final List<MovementStrategy.Direction> directions;
+    private final MovementStrategy.Direction direction;
     private final MovementStrategy movementStrategy;
     private static final int STEPS = 1;
 
     /**
      * Constructor a new command for moving the player in multiple directions.
      * @param player player to move
-     * @param directions list of directions where to move
+     * @param direction the direction where to move
      * @param movementStrategy the strategy used to calculate and validate the movement
      */
-    public MoveInCombinedDirectionsCommand(final Player player, final List<MovementStrategy.Direction> directions,
+    public MoveInCombinedDirectionsCommand(final Player player, final MovementStrategy.Direction direction,
                                         final MovementStrategy movementStrategy) {
         this.player = player;
-        this.directions = Collections.unmodifiableList(directions);
+        this.direction = direction;
         this.movementStrategy = movementStrategy;
     }
 
     @Override
     public void execute() {
-        for (final var direction : directions) {
-            final MovementCommand singleMovementCommand = new MoveInSingleDirection(
-                (MutablePlayer) this.player, STEPS, direction, movementStrategy);
+        final MovementCommand singleMovementCommand = new MoveInSingleDirection(
+            (MutablePlayer) this.player, STEPS, direction, movementStrategy);
             singleMovementCommand.execute();
-        }
     }
  
 }
