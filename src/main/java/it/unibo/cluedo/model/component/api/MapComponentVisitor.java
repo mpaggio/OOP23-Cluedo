7e20369882a69e72eb0c3a702623bcc2aeebd6f8
Marco Paggetti <marco.paggetti@studio.unibo.it>
2024-08-27 15:50:13 +0200

    Added some utility methods in Visitor

--- src/main/java/it/unibo/cluedo/model/component/api/MapComponentVisitor.java
+++ src/main/java/it/unibo/cluedo/model/component/api/MapComponentVisitor.java
@@ -1,9 +1,11 @@
 package it.unibo.cluedo.model.component.api;
 
 import java.util.List;
+import java.util.Optional;
 
 import it.unibo.cluedo.model.room.api.Room;
 import it.unibo.cluedo.model.square.api.Square;
+import it.unibo.cluedo.utilities.Position;
 
 /**
  * Interface made for pattern visitor of the map component.
@@ -36,4 +38,28 @@ public interface MapComponentVisitor {
      * @return the list of visited squares
      */
     List<Square> getVisitedSquare();
+
+    /**
+     * Gets the square by the specified position.
+     * 
+     * @param position the position of the square to find
+     * @return the square with the specified position
+     */
+    Square getSquareByPosition(Position position);
+
+    /**
+     * Check if the given square is part of a room.
+     * 
+     * @param square the square to be checked
+     * @return true if the square is part of a room, false otherwise
+     */
+    boolean isSquareInRoom(Square square);
+
+    /**
+     * Gets the room having the specified square if exists.
+     * 
+     * @param square the square to check
+     * @return the room with the square if present 
+     */
+    Optional<Room> getRoomBySquare(Square square);
 }
