c6bf79296d8ff709e3b2207fc7cebbf96d984e0e
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-08-27 14:13:54 +0200

    Added a test class for GameModelBuilderImpl, not already implemented

---
+++ src/test/java/it/unibo/model/gamemodelbuilder/GameModelBuilderImplTest.java
@@ -0,0 +1,8 @@
+package it.unibo.model.gamemodelbuilder;
+
+import it.unibo.cluedo.model.GameModelBuilderImpl;
+/**
+ * Test class for a {@link GameModelBuilderImpl} class.
+ */
+public class GameModelBuilderImplTest {
+}
