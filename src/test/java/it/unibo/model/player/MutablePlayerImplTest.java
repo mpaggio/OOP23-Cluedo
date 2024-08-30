ec1ef23e8e99404b29daa09e8d81978ea5f242df
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-08-30 15:42:39 +0200

    Added a first implementation of MutablePlayerImplTest class

--- src/test/java/it/unibo/model/player/MutablePlayerImplTest.java
+++ src/test/java/it/unibo/model/player/MutablePlayerImplTest.java
@@ -1,8 +1,30 @@
 package it.unibo.model.player;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import it.unibo.cluedo.model.player.impl.MutablePlayerImpl;
 
 /**
  * Test class for a {@link MutablePlayerImpl} class.
  */
-public class MutablePlayerImplTest {
+final class MutablePlayerImplTest {
+
+    private MutablePlayerImpl player;
+    private static final String PLAYER_1 = "Player1";
+    private static final String COLOR_RED = "Red";
+    
+    @BeforeEach
+    void setUp() {
+        player = new MutablePlayerImpl(PLAYER_1, COLOR_RED);
+    }
+
+    @Test
+    void testInitialValues() {
+        assertEquals(PLAYER_1, player.getUsername());
+        assertEquals(COLOR_RED, player.getColor());
+
+    }
 }
