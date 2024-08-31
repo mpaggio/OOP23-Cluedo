7fe28b462538a1c4c47364399ab35d767229ee82
Federico Brighi <federico.brighi2@studio.unibo.it>
2024-08-31 15:57:26 +0200

    Changed playerColor type from java.awt.Color in String,modified implementation and tests

--- src/test/java/it/unibo/controller/gamemenucontroller/GameMenuControllerImplTest.java
+++ src/test/java/it/unibo/controller/gamemenucontroller/GameMenuControllerImplTest.java
@@ -4,14 +4,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 import java.util.Arrays;
-import java.awt.Color;
-//import java.lang.reflect.Field;
 import it.unibo.cluedo.controller.gamemenucontroller.api.GameMenuController;
 import it.unibo.cluedo.controller.gamemenucontroller.impl.GameMenuControllerImpl;
 import it.unibo.cluedo.model.player.api.Player;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
-//import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
 /**
@@ -22,6 +20,9 @@ class GameMenuControllerImplTest {
     private static final String PLAYER1 = "Player1";
     private static final String PLAYER2 = "Player2";
     private static final String PLAYER3 = "Player3";
+    private static final String RED = "RED";
+    private static final String GREEN = "GREEN";
+    private static final String BLUE = "BLUE";
 
     @BeforeEach
     void setUp() {
@@ -31,54 +32,45 @@ class GameMenuControllerImplTest {
     @Test
     void testStartGameWithInvalidInputs() {
         final List<String> playerUsernames = Arrays.asList(PLAYER1, PLAYER2, PLAYER3);
-        final List<Color> playerColors = Arrays.asList(Color.RED, Color.GREEN, Color.BLUE);
+        final List<String> playerColors = Arrays.asList(RED, GREEN, BLUE);
         assertTrue(gameMenuController.startGame(playerUsernames, playerColors));
     }
 
     @Test
     void testStartGameWithInvalidNumberOfPlayers() {
         final List<String> playerUsernames = Arrays.asList(PLAYER1, PLAYER2);
-        final List<Color> playerColors = Arrays.asList(Color.RED, Color.GREEN, Color.BLUE);
+        final List<String> playerColors = Arrays.asList(RED, GREEN, BLUE);
         assertFalse(gameMenuController.startGame(playerUsernames, playerColors));
     }
 
     @Test
     void testStartGameWithDuplicateNames() {
         final List<String> playerUsernames = Arrays.asList(PLAYER1, PLAYER1, PLAYER3);
-        final List<Color> playerColors = Arrays.asList(Color.RED, Color.GREEN, Color.BLUE);
+        final List<String> playerColors = Arrays.asList(RED, GREEN, BLUE);
         assertFalse(gameMenuController.startGame(playerUsernames, playerColors));
     }
 
     @Test
     void testStartGameWithDuplicateColors() {
         final List<String> playerUsernames = Arrays.asList(PLAYER1, PLAYER2, PLAYER3);
-        final List<Color> playerColors = Arrays.asList(Color.RED, Color.RED, Color.BLUE);
+        final List<String> playerColors = Arrays.asList(RED, RED, BLUE);
         assertFalse(gameMenuController.startGame(playerUsernames, playerColors));
     }
 
-    /**
+
     @Test
     void testSetPlayers() {
         final List<String> playerUsernames = Arrays.asList(PLAYER1, PLAYER2, PLAYER3);
-        final List<Color> playerColors = Arrays.asList(Color.RED, Color.GREEN, Color.BLUE);
+        final List<String> playerColors = Arrays.asList(RED, GREEN, BLUE);
         gameMenuController.setPlayer(playerUsernames, playerColors);
         final List<Player> players = gameMenuController.getPlayers();
         assertEquals(players.size(), 3);
         for (int i = 0; i < players.size(); i++) {
             assertEquals(playerUsernames.get(i), players.get(i).getUsername());
-            Color expectedColor = playerColors.get(i);
-            Color actualColor = null;
-            try {
-                String colorName = players.get(i).getColor().toUpperCase();
-                Field field = Color.class.getField(colorName);
-                actualColor = (Color) field.get(null);
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                actualColor = null;
-            }
-            assertEquals(expectedColor, actualColor);
+            assertEquals(playerColors.get(i), players.get(i).getColor());
         }
     }
-    */
+
 
     @Test
     void testGetPlayersWithouSettingPlayers() {
