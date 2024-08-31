dfe8a747864d96711ee9e8ff351f32168dbcad14
Marco Paggetti <marco.paggetti@studio.unibo.it>
2024-08-31 17:01:55 +0200

    Added first version of MapView implementation and controller

---
+++ src/main/java/it/unibo/cluedo/application/Cluedo.java
@@ -0,0 +1,39 @@
+package it.unibo.cluedo.application;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+import it.unibo.cluedo.view.MapView;
+
+/**
+ * The main class for the Cluedo application.
+ */
+public class Cluedo {
+    /**
+     * Private constructor to prevent instantiation.
+     */
+    private Cluedo() {
+    }
+
+    /**
+     * The main method to start the Cluedo application.
+     * 
+     * @param args the command line arguments (not used)
+     */
+    public static void main(final String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                final JFrame frame = new JFrame();
+                final MapView map = new MapView();
+                //final Dimension screenDimension = Toolkit.getDefaultToolkit().getScreenSize();
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.add(map);
+                //frame.setSize(screenDimension);
+                //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        });
+    }
+}
