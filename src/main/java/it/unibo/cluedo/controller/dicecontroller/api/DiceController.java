14220e94f34c4ad925c3d7af5d1b200eacfe10cf
Federico Brighi <federico.brighi2@studio.unibo.it>
2024-09-02 22:00:42 +0200

    Started the implementation of the DiceController

---
+++ src/main/java/it/unibo/cluedo/controller/dicecontroller/api/DiceController.java
@@ -0,0 +1,12 @@
+package it.unibo.cluedo.controller.dicecontroller.api;
+
+/**
+ * Interface for the controller class that handles the dice logic.
+ */
+public interface DiceController {
+
+    /**
+     * Paints the result of the dice roll.
+     */
+    void paintdICE(int result);
+}
