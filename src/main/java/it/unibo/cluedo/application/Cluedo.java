526a86f96a92b17bb5404d4201aedaa83e5643f5
Federico Brighi <federico.brighi2@studio.unibo.it>
2024-09-02 21:45:46 +0200

    Fixed private in public

--- src/main/java/it/unibo/cluedo/application/Cluedo.java
+++ src/main/java/it/unibo/cluedo/application/Cluedo.java
@@ -6,7 +6,7 @@ import it.unibo.cluedo.controller.maincontroller.impl.MainControllerImpl;
  * The main class for the Cluedo application.
  */
 public final class Cluedo {
-    private static final MainControllerImpl CONTROLLER = new MainControllerImpl();
+    public static final MainControllerImpl CONTROLLER = new MainControllerImpl();
     /**
      * Private constructor to prevent instantiation.
      */
@@ -15,7 +15,7 @@ public final class Cluedo {
 
     /**
      * The main method to start the Cluedo application.
-     * 
+     *
      * @param args the command line arguments (not used)
      */
     public static void main(final String[] args) {
