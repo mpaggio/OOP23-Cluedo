a86648cbb227c497f0271792970f210e97eafd5e
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2024-09-03 17:32:07 +0200

    Created the interface FinalAccusationController

---
+++ src/main/java/it/unibo/cluedo/controller/accusationcontroller/api/FinalAccusationController.java
@@ -0,0 +1,18 @@
+package it.unibo.cluedo.controller.accusationcontroller.api;
+
+import it.unibo.cluedo.model.card.api.Card;
+
+public interface FinalAccusationController {
+    /**
+     * Method that allows the player to make a final accusation.
+     * @param suspect the suspect card.
+     * @param weapon the weapon card.
+     * @param room the room card.
+     */
+    void makeFinalAccusation(Card suspect, Card weapon, Card room);
+    /**
+     * Method that checks if the final accusation made by the player is correct.
+     * @return true if the accusation is correct, false otherwise.
+     */
+    boolean isFinalAccusationCorrect();
+}
