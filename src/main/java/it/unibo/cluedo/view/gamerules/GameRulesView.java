b24eb0372f55a5bd7b94b04e4c587542d95018a0
Marco Paggetti <marco.paggetti@studio.unibo.it>
2024-09-04 11:17:13 +0200

    Resolved unused import file

--- src/main/java/it/unibo/cluedo/view/gamerules/GameRulesView.java
+++ src/main/java/it/unibo/cluedo/view/gamerules/GameRulesView.java
@@ -8,7 +8,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.JOptionPane;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
