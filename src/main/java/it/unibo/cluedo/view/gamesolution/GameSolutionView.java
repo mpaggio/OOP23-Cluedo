682197e0a912a33acf0a60fc35307fb5f654ab12
Kevin Shimaj <kevin.shimaj@studio.unibo.it>
2024-09-08 16:41:54 +0200

    Modified test method for a saved game

--- src/main/java/it/unibo/cluedo/view/gamesolution/GameSolutionView.java
+++ src/main/java/it/unibo/cluedo/view/gamesolution/GameSolutionView.java
@@ -12,13 +12,15 @@ import javax.swing.BorderFactory;
  */
 public class GameSolutionView extends JPanel {
     private static final long serialVersionUID = 1L;
+    private static final int HGAP = 20;
+    private static final int VGAP = 0;
 
     /**
      * Constructor for the GameSolutionView class.
      * @param cardInfo the info about the cards 
      */
     public GameSolutionView(final List<String> cardInfo) {
-        setLayout(new FlowLayout(FlowLayout.CENTER, 20, 0));
+        setLayout(new FlowLayout(FlowLayout.CENTER, HGAP, VGAP));
         setBorder(BorderFactory.createTitledBorder("Game Solution"));
         for (final String path : cardInfo) {
             final ImageIcon image = new ImageIcon(path);
