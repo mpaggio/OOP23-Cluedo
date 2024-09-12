c30412953ee26bfda3b37e072651f89bcbeb7865
Mattia Saponaro <mattia.saponaro2@studio.unibo.it>
2024-09-12 10:02:53 +0200

    Added the test for the method rollDice() which verify both the case of start of the turn and double roll dice. The gamemodel correctily sets the turnfases as expected.

--- src/test/java/it/unibo/model/gamemodel/GameModelTest.java
+++ src/test/java/it/unibo/model/gamemodel/GameModelTest.java
@@ -1,15 +1,20 @@
 package it.unibo.model.gamemodel;
 
-import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import it.unibo.cluedo.model.GameModel;
 import it.unibo.cluedo.model.GameModelBuilder;
 import it.unibo.cluedo.model.GameModelBuilderImpl;
+import it.unibo.cluedo.model.player.api.MutablePlayer;
+import it.unibo.cluedo.utilities.TurnFase;
 
 final class GameModelTest {
     
+    private static final int MAX_DICE_RESULT = 6;
+
     private GameModel gameModel;
 
     @BeforeEach
@@ -18,7 +23,24 @@ final class GameModelTest {
         builder.addPlayer("player1", "red");
         builder.addPlayer("player2", "blue");
         builder.addPlayer("player3", "green");
+        builder.withGameSolution();
         gameModel = builder.build();
     }
 
+    @Test
+    void testRollDice() {
+        assertTrue(gameModel.getTurnFase().equals(TurnFase.ROLL_DICE));
+        gameModel.rollDice();
+        assertTrue(gameModel.getDiceResult() > 0
+            && gameModel.getDiceResult() <= MAX_DICE_RESULT);
+        assertTrue(gameModel.getTurnFase().equals(TurnFase.DRAW_UNFORESEEN));
+        gameModel.endTurn();
+        if (gameModel.getCurrentPlayer() instanceof MutablePlayer) {
+            ((MutablePlayer) gameModel.getCurrentPlayer()).setDoubleRollDice(true);
+        }
+        gameModel.rollDice();
+        assertTrue(gameModel.getDiceResult() > 0
+            && gameModel.getDiceResult() <= MAX_DICE_RESULT);
+        assertTrue(gameModel.getTurnFase().equals(TurnFase.MOVE_PLAYER));
+    }
 }
